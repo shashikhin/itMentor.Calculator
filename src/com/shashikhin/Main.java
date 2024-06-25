0eda801f9b707397e4d58d06dc1f6658f4087f3a
Shashikhin <shashihinv@gmail.com>
2024-06-26 02:45:34 +0300

    Initial commit

---
+++ src/com/shashikhin/Main.java
@@ -0,0 +1,63 @@
+package com.shashikhin;
+
+import java.io.IOException;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+//        //для тестирования
+//        String result;
+//        try {
+//        System.out.println(calc("3 - 10"));
+//        }
+//        catch (IOException e) {
+//            System.out.println("Неверный ввод");
+//        }
+    }
+
+    private static boolean IsNumberValid(String inputNumber)
+    {
+        int number;
+
+        try {
+            number = Integer.parseInt(inputNumber);
+        }
+        catch ( NumberFormatException exception ) {
+            return false;
+        }
+
+        return number >= 1 && number <= 10;
+    }
+
+    public static String calc(String input) throws IOException
+    {
+        String result;
+        int firstNumber;
+        int secondNumber;
+
+        String[] operators = input.split(" ");
+
+        if (operators.length != 3) throw new IOException();
+
+        boolean check = IsNumberValid(operators[0]);
+
+        if (!IsNumberValid(operators[0]) || !IsNumberValid(operators[2]))
+        {
+            throw new IOException();
+        }
+
+        firstNumber = Integer.parseInt(operators[0]);
+        secondNumber = Integer.parseInt(operators[2]);
+
+        result = switch (operators[1]) {
+            case "+" -> String.valueOf(firstNumber + secondNumber);
+            case "-" -> String.valueOf(firstNumber - secondNumber);
+            case "*" -> String.valueOf(firstNumber * secondNumber);
+            case "/" -> String.valueOf(firstNumber / secondNumber);
+            default -> throw new IOException();
+        };
+
+        return result;
+    }
+}
